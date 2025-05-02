6ee6ebbaaaaccb9fedafd4834eed41951ac07409
komsky <komsky@gmail.com>
2025-05-02 14:03:21 +0100

    missed line

--- tests/list_audo_devices.py
+++ tests/list_audo_devices.py
@@ -2,4 +2,5 @@ import sounddevice as sd
 print(sd.query_devices())
 
 for idx, dev in enumerate(sd.query_devices()):
-    if dev['max_output_channels'] >= 2
+    if dev['max_output_channels'] >= 2:
+        print(idx, dev['name'])
