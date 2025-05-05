536ae885502092827aa033400c539824106878d4
komsky <komsky@gmail.com>
2025-05-05 18:35:47 +0100

    converted mp3 to 16k wav

--- tests/convert_mp3.py
+++ tests/convert_mp3.py
@@ -1,18 +1,20 @@
 import os
 import subprocess
+core_path = os.path.dirname(os.path.abspath(__file__))
+parent_path = os.path.dirname(core_path)
 
 def convert_mp3_to_wav(root_dir):
     for dirpath, _, filenames in os.walk(root_dir):
         for filename in filenames:
             if filename.lower().endswith('.mp3'):
                 mp3_path = os.path.join(dirpath, filename)
-                wav_path = os.path.splitext(mp3_path)[0] + '.wav'
+                wav_path = os.path.splitext(mp3_path)[0] + '_16k.wav'
 
                 # Convert using ffmpeg to 16-bit PCM WAV
                 try:
                     subprocess.run(
-                        ['ffmpeg', '-y', '-i', mp3_path, '-acodec', 'pcm_s16le', wav_path],
-                        check=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL
+                        ['ffmpeg','-y', '-i', mp3_path,'-ar','16000', '-acodec', 'pcm_s16le', wav_path],
+                        check=True
                     )
                     os.remove(mp3_path)
                     print(f"Converted and removed: {mp3_path}")
@@ -20,4 +22,4 @@ def convert_mp3_to_wav(root_dir):
                     print(f"Failed to convert: {mp3_path}")
 
 if __name__ == "__main__":
-    convert_mp3_to_wav("/home/komsky/UselessBox/uselessbox/audio/")
+    convert_mp3_to_wav(os.path.join(parent_path, "audio/"))
