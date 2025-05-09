fc78287d80940abab6e57a361c020b4e65e88095
komsky <komsky@gmail.com>
2025-05-09 17:39:50 +0100

    wez przestan

--- main.py
+++ main.py
@@ -208,13 +208,13 @@ class MainApplication:
         
     def WindowsXP(self):
         wsled.on()
-        sounds.play_sound("audio/windows_startup.wav")
+        sounds.play("audio/windows_startup.wav")
         self.topServo.up()   
         time.sleep(1)
         self.handServo.turnOffToggleAndBack()  
         wsled.off()
         self.handServo.zero()
-        sounds.play_sound("audio/windows_startup.wav")
+        sounds.play("audio/windows_startup.wav")
         time.sleep(1.5)
         self.topServo.zero()        
         time.sleep(1)
