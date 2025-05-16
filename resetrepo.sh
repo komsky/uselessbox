f388fad5ac8113f549703da966320a3b981f1d67
komsky <komsky@gmail.com>
2025-05-16 12:41:36 +0100

    resetfix

--- resetrepo.sh
+++ resetrepo.sh
@@ -2,5 +2,5 @@ git reset --hard HEAD
 git pull https://github.com/komsky/uselessbox.git
 chmod +x resetrepo.sh
 chmod +x start.sh
-chmod +x install_uselessbox_service.sh.sh
+chmod +x install_uselessbox_service.sh
 chmod +x stop_uselessbox_service.sh
